66a635c05e8a63a2ec1aa1a957d5fba7f52f079f
User <user@Users-MacBook-Pro.local>
2022-08-17 21:22:49 +0100

    Done

---
+++ share-slide.js
@@ -0,0 +1,17 @@
+
+const bestSlide = (n) => {
+    let result = 0;
+    let counter = 1;
+    for(let i=1; i<=n; i++){
+        result += counter;
+        for(let j=1; j<=i; j++){
+            process.stdout.write(`${counter} `);
+            counter++;
+        }
+        console.log("");
+        
+    }
+    console.log("");
+    return result;
+}
+console.log("Answer: ", bestSlide(10));
